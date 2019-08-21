15fea43c84787a0d7cf253fac17a3303944eaacd
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 13:41:22 +0600

    Create index.blade.php

---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.customer')
+@section('content')
+        <!-- partial -->
+        <div class="main-panel">
+          
+          </div>
+  @endsection
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+  
+
+  @section('title')
+    Customer Homepage
+  @endsection
