2f53e726495d0bbb40d0a3ac51a043e0ef38d671
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 14:29:22 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-
+Route::get('/','homeController@index')->name('home');
 Route::get('/signin', 'loginController@index')->name('login');
 Route::post('/signin', 'loginController@verify');
 
