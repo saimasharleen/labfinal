a7fa5491ce8fd6e96346e48985ad0bb7ceb93105
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 12:15:04 +0600

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -9,31 +9,26 @@ use Illuminate\Http\Request;
 class adminController extends Controller
 {
     public function index(){
-    /*if(session('email') == null){
+    if(session('email') == null){
            return redirect()->route('login');
         }
     else{
         $user = Userinfo::where('email',session('email'))->first();
 
          return view('admin.index',compact('user'));
-        }*/
-/*        $user = Userinfo::where('email',session('email'))->first();
+        }
 
-         return view('admin.index',compact('user'));*/
-         return view('admin.index');
     }
     public function signup()
     {
-       /* if(session('email') == null){
+        if(session('email') == null){
         }
     else{
            return redirect()->route('login');
         $user = Userinfo::where('email',session('email'))->first();
          return view('admin.pages.signup',compact('user'));
-        }*/
-         /*$user = Userinfo::where('email',session('email'))->first();
-         return view('admin.pages.signup',compact('user'));*/
-         return view('admin.pages.signup');
+        }
+         
     }
      public function store(Request $request)
     {
