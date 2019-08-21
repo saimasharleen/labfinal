36b0d1abbc9cdc6744ea295c2148b60534e417b6
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 13:20:44 +0600

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use DB; 
 use App\Userinfo;
 use App\login;
+use App\pinfo;
 use Illuminate\Http\Request;
 
 class adminController extends Controller
@@ -59,5 +60,30 @@ class adminController extends Controller
         $userlogin ->save();
 
         return redirect()->route('signup')->with('message','registered successfully');;
+    }
+    public function pinfoindex()
+    {   if(session('email') == null){
+           return redirect()->route('login');
+        }
+    else{
+        $user = Userinfo::where('email',session('email'))->first();
+        $userdata = Userinfo::where('usertype', 'admin')->get();
+        return view('admin.pages.pinfo',compact(['user','userdata']));
+        }
+        
+    }
+    
+     public function pinfostore(Request $request)
+    {
+        $this->validate($request, [
+            'name'=>'Required',
+            'price'=>'Required'
+        ]);
+        $user = new notice();
+        $user -> postd     = $request->postd;
+        $user ->save();
+
+
+        return redirect()->route('notice')->with('message','notice added successfully');;
     }
 }
