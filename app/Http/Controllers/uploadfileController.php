49e0d32fe974a9db90b1d4ef30b6bdaca2e176e0
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 13:03:35 +0600

    Update uploadfileController.php

--- app/Http/Controllers/uploadfileController.php
+++ app/Http/Controllers/uploadfileController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers;
 use App\Userinfo;
 use Illuminate\Http\Request;
-use App\;
+use App\productspost;
 
 class uploadfileController extends Controller
 {
-    //
+    public function productspost()
+    {   $user = Userinfo::where('email',session('email'))->first();
+        return view('admin.pages.productspost',compact('user'));
+    }
+    public function store(Request $request)
+    {
+		$this->validate($request, [
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    	]);
+
+    if ($request->hasFile('image')) {
+        $image = $request->file('image');
+        $name = time().'.'.$image->getClientOriginalExtension();
+        $destinationPath = public_path('/images');
+        $image->move($destinationPath, $name);
+        //$this->save();
+
+        $adv  = new productspost([
+            'image' => $name
+        ]);
+        $adv->save();
+
+         return back();
+        
+    }
+
+    }
 }
