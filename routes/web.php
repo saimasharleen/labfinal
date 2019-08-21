8590bb40cc651cd64db3bd59b2222b40647da077
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 12:32:53 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,10 @@ Route::post('/signin', 'loginController@verify');
 Route::get('/admin/index', 'adminController@index')->name('admin.index');
 Route::get('/admin/pages/signup','adminController@signup')->name('signup');
 Route::post('/admin/pages/signup','adminController@store');
-Route::get('/admin/pages/banuser', 'adminController@banuser')->name('banuser');
+/*Route::get('/admin/pages/banuser', 'adminController@banuser')->name('banuser');
 Route::get('/admin/pages/userlist', 'adminController@userlist')->name('userlist');
 Route::get('/admin/pages/userlist/block/{id}', 'adminController@block')->name('userlist.block');
-Route::get('/admin/pages/userlist/unblock/{id}', 'adminController@unblock')->name('userlist.unblock');
+Route::get('/admin/pages/userlist/unblock/{id}', 'adminController@unblock')->name('userlist.unblock');*/
 
 
 Route::get('/logout', 'logoutController@index')->name('logout');
