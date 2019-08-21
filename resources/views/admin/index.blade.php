7486ce7d935f7d9c31ec14280cb6a2f50ceeb99d
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 10:22:58 +0600

    Create index.blade.php

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.superadmin')
+@section('content')
+        <!-- partial -->
+        <div class="main-panel">
+          
+          </div>
+  @endsection
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+  
+
+  @section('title')
+    SuperAdmin Homepage
+  @endsection
