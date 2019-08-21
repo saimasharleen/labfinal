fe265954fea8f9f89d075fc4688ff5084a901811
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 13:57:05 +0600

    Update customerController.php

--- app/Http/Controllers/customerController.php
+++ app/Http/Controllers/customerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use DB; 
 use App\Userinfo;
 use App\login;
-
+use App\preview;
 use Illuminate\Http\Request;
 
 class customerController extends Controller
@@ -18,5 +18,31 @@ class customerController extends Controller
 
          return view('customer.index',compact('user'));
         }
+    }
+    public function previewindex()
+    {   if(session('email') == null){
+           return redirect()->route('login');
+        }
+    else{
+        $user = Userinfo::where('email',session('email'))->first();
+        $userdata = Userinfo::where('usertype', 'customer')->get();
+        return view('customer.pages.preview',compact(['user','userdata']));
+        }
+        
+    }
+    
+     public function previewstore(Request $request)
+    {
+        $this->validate($request, [
+            'name'=>'Required',
+            'comment'=>'Required'
+        ]);
+        $user = new preview();
+        $user -> name     = $request->name;
+        $user -> comment     = $request->comment;
+        $user ->save();
+
+
+        return redirect()->route('preview')->with('message','Review added successfully');;
     }
 }
