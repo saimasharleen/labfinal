b5185522867409ac741b9df307ebbf0f9139fe1b
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 13:03:38 +0600

    Update productspost.php

--- app/productspost.php
+++ app/productspost.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class productspost extends Model
 {
-    protected $table      = 'productspost';
-    protected $primaryKey = 'id';
-	public    $timestamps = false;
-	protected $fillable = ['img'];
+    protected $table = 'productspost';
+    protected $primaryKey ='id' ;
+    protected $fillable = ['image'];
 }
