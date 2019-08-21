6930c6515eaca1a94e0dbcb03f080c1b4a3971c2
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 10:13:27 +0600

    Update logoutController.php

--- app/Http/Controllers/logoutController.php
+++ app/Http/Controllers/logoutController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class logoutController extends Controller
 {
-    //
+	public function index(Request $request){
+
+    	$request->session()->flush();
+    	return redirect('/signin');
+    }
+    
 }
