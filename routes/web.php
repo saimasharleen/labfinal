d154ecef7e6b3f0dc8f92a7d5679fa2fe91babb9
saimasharleen <saimasharleen0@gmail.com>
2019-08-21 13:03:42 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::post('/signin', 'loginController@verify');
 Route::get('/admin/index', 'adminController@index')->name('admin.index');
 Route::get('/admin/pages/signup','adminController@signup')->name('signup');
 Route::post('/admin/pages/signup','adminController@store');
+
+Route::get('/admin/pages/productspost', 'uploadfileController@productspost');
+Route::post('/admin/pages/productspost', 'uploadfileController@store');
 /*Route::get('/admin/pages/banuser', 'adminController@banuser')->name('banuser');
 Route::get('/admin/pages/userlist', 'adminController@userlist')->name('userlist');
 Route::get('/admin/pages/userlist/block/{id}', 'adminController@block')->name('userlist.block');
